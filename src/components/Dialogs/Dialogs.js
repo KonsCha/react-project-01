9460b5214755e1ef3f1d6afd3c165085a496d7fb
KonsCha <deathmorning@mail.ru>
2021-12-03 22:05:51 +0300

    добавил обработчик событий onClick на кнопку. С помощью ref создал ссылку на элементtextarea. при наборе текста в textarea и нажатие на кнопку, текст выводится в алерт.

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,10 +1,18 @@
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
     let DialogItems = props.state.DialogsData.map(item => <DialogItem name={item.name} id={item.id}/>);
     let MessageItems = props.state.MessageData.map(item => <Message message={item.message}/>);
+
+    let newTextElement = React.createRef();
+
+    let addText = () => {
+        let text = newTextElement.current.value;
+        alert(text);
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -12,6 +20,12 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {MessageItems}
+                <div>
+                    <textarea ref={newTextElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addText}>Add some text</button>
+                </div>
             </div>
         </div>
     )
--- src/components/MyPosts/MyPosts.js
+++ src/components/MyPosts/MyPosts.js
@@ -1,17 +1,24 @@
 import Posts from './Posts/Posts'
 import s from './MyPosts.module.css';
+import React from 'react';
 
 const MyPosts = (props) => {
     let PostsItem = props.state.PostsData.map(item => <Posts counter={item.counter} message={item.message}
                                                              src={item.src}/>);
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
     return (
         <div className={s.MyPosts}>
             <div>
                 <div className={s.textarea}>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div className={s.buttonIn}>
-                    <button className={s.button}>Add Post</button>
+                    <button className={s.button} onClick={addPost}>Add Post</button>
                 </div>
                 {PostsItem}
             </div>
